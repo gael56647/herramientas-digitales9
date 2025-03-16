f31333871c08c729ab3e5ab111f6c54062bdcd88
gael56647 <amayagael877@gmail.com>
2025-03-15 20:13:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Botón de Modo Oscuro
+const botonModoOscuro = document.getElementById('modoOscuro');
+
+botonModoOscuro.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+// Validación del Formulario de Contacto
+document.addEventListener("DOMContentLoaded", function () {
+    let botonModoOscuro = document.getElementById("modoOscuro");
+
+    if (botonModoOscuro) {  // Solo ejecuta si el botón existe
+        botonModoOscuro.addEventListener("click", function() {
+            // Código para cambiar el tema
+        });
+    }
+});
